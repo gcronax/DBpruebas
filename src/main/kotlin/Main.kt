56b0643330a79b4e344257d9dae1d0453b5c6274
guicamher <guillecampher15@gmail.com>
2025-10-23 20:03:52 +0200

    si

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,9 @@ import java.sql.DriverManager
 import java.sql.SQLException
 
 // Ruta al archivo de base de datos SQLite
-const val URL_BD = "jdbc:sqlite:src/main/resources/florabotanica.sqlite"
+const val URL_BD = "jdbc:sqlite:src/main/resources/cars.sqlite"
+const val URL_BD_test = "jdbc:sqlite:src/main/resources/cars.sqlite"
+
 
 // Obtener conexión
 fun getConnection(): Connection? {
@@ -19,40 +21,40 @@ fun main() {
 
     // Listar todas las plantas
     println("Lista de plantas:")
-    PlantasDAO.listarPlantas().forEach {
-        println(" - [${it.id_planta}] ${it.nombreComun} (${it.nombreCientifico}), stock ${it.stock} unidades, precio: ${it.precio} €")
+    CochesDAO.listarCoches().forEach {
+        println(" - [${it.id_coche}] ${it.modelo}, ${it.marca}, ${it.consumo}, ${it.hp} ")
     }
 
     // Consultar planta por ID
-    val planta = PlantasDAO.consultarPlantaPorId(3)
-    if (planta != null) {
-        println("Planta encontrada: [${planta.id_planta}] ${planta.nombreComun} (${planta.nombreCientifico}), stock ${planta.stock} unidades, precio: ${planta.precio} €")
-    } else {
-        println("No se encontró ninguna planta con ese ID.")
-    }
-
-
-    // Insertar plantas
-    PlantasDAO.insertarPlanta(
-        Planta(
-            nombreComun = "Palmera",
-            nombreCientifico = "Arecaceae",
-            stock = 2,
-            precio = 50.5
-        )
-    )
-
-    // Actualizar planta con id=1
-    PlantasDAO.actualizarPlanta(
-        Planta(
-            id_planta = 1,
-            nombreComun = "Aloe Arborescens",
-            nombreCientifico = "Aloe barbadensis miller",
-            stock = 20,
-            precio = 5.8
-        )
-    )
-
-    // Eliminar planta con id=2
-    PlantasDAO.eliminarPlanta(2)
+//    val planta = PlantasDAO.consultarPlantaPorId(3)
+//    if (planta != null) {
+//        println("Planta encontrada: [${planta.id_planta}] ${planta.nombreComun} (${planta.nombreCientifico}), stock ${planta.stock} unidades, precio: ${planta.precio} €")
+//    } else {
+//        println("No se encontró ninguna planta con ese ID.")
+//    }
+//
+//
+//    // Insertar plantas
+//    PlantasDAO.insertarPlanta(
+//        Planta(
+//            nombreComun = "Palmera",
+//            nombreCientifico = "Arecaceae",
+//            stock = 2,
+//            precio = 50.5
+//        )
+//    )
+//
+//    // Actualizar planta con id=1
+//    PlantasDAO.actualizarPlanta(
+//        Planta(
+//            id_planta = 1,
+//            nombreComun = "Aloe Arborescens",
+//            nombreCientifico = "Aloe barbadensis miller",
+//            stock = 20,
+//            precio = 5.8
+//        )
+//    )
+//
+//    // Eliminar planta con id=2
+//    PlantasDAO.eliminarPlanta(2)
 }
--- src/main/kotlin/prueba.kt
+++ src/main/kotlin/prueba.kt
@@ -1,3 +1,5 @@
+import java.sql.SQLException
+
 data class Coche(
     val id_coche: Int? = null, // lo genera SQLite automáticamente
     val modelo: String,
@@ -8,16 +10,54 @@ data class Coche(
 data class Repuesto(
     val id_repuesto: Int? = null, // lo genera SQLite automáticamente
     val localizacion: String,
-    val precio: Double
+    val precio: Double,
+    var cantidad: Int,
+    val nombre: String
 )
 data class Rueda(
     val id_rueda: Int? = null, // lo genera SQLite automáticamente
     val tipo: String,
     val precio: Double,
-    val pulgadas: Double
-    )
+    val pulgadas: Double,
+    var cantidad: Int
+)
 
 object CochesDAO {
+
+    fun crearCoche(id_coche: Int, id_repuesto: Int, cantidad: Int) {
+        getConnection()?.use { conn ->
+            try {
+                conn.autoCommit = false  // Iniciar transacción manual
+
+                // Restar stock a la planta
+//                conn.prepareStatement("UPDATE plantas SET stock = stock - $cantidad WHERE id_planta = ?").use { stock ->
+//                    stock.setInt(1, id_planta)
+//                    stock.executeUpdate()
+//                }
+//
+//                // Añadir línea en tabla jardines_plantas
+//                conn.prepareStatement("INSERT INTO jardines_plantas(id_jardin, id_planta, cantidad) VALUES (?, ?, ?)").use { plantar ->
+//                    plantar.setInt(1, id_jardin)
+//                    plantar.setInt(2, id_planta)
+//                    plantar.setInt(3, cantidad)
+//                    plantar.executeUpdate()
+//                }
+
+                // Confirmar cambios
+                conn.commit()
+                println("Transacción realizada con éxito.")
+            } catch (e: SQLException) {
+                if (e.message?.contains("UNIQUE constraint failed") == true) {
+                    println("Error: intento de insertar clave duplicada")
+                    conn.rollback()
+                    println("Transacción revertida.")
+                } else {
+                    throw e // otros errores, relanzamos
+                }
+            }
+        }
+    }
+
 
     fun listarCoches(): List<Coche> {
         val lista = mutableListOf<Coche>()
@@ -40,7 +80,6 @@ object CochesDAO {
         return lista
     }
 
-    // Consultar planta por ID
     fun consultarCochePorId(id: Int): Coche? {
         var coche: Coche? = null
         getConnection()?.use { conn ->
@@ -49,11 +88,11 @@ object CochesDAO {
                 val rs = pstmt.executeQuery()
                 if (rs.next()) {
                     coche = Coche(
-                        id_planta = rs.getInt("id_planta"),
-                        nombreComun = rs.getString("nombre_comun"),
-                        nombreCientifico = rs.getString("nombre_cientifico"),
-                        stock = rs.getInt("stock"),
-                        precio = rs.getDouble("precio")
+                        id_coche = rs.getInt("id_coche"),
+                        modelo = rs.getString("modelo"),
+                        marca = rs.getString("marca"),
+                        consumo = rs.getDouble("consumo"),
+                        hp = rs.getInt("hp")
                     )
                 }
             }
@@ -61,54 +100,247 @@ object CochesDAO {
         return coche
     }
 
-    fun insertarPlanta(planta: Coche) {
+    fun insertarCoche(coche: Coche) {
         getConnection()?.use { conn ->
             conn.prepareStatement(
-                "INSERT INTO plantas(nombre_comun, nombre_cientifico, stock, precio) VALUES (?, ?, ?, ?)"
+                "INSERT INTO coches(modelo, marca, consumo, hp) VALUES (?, ?, ?, ?)"
             ).use { pstmt ->
-                pstmt.setString(1, planta.nombreComun)
-                pstmt.setString(2, planta.nombreCientifico)
-                pstmt.setInt(3, planta.stock)
-                pstmt.setDouble(4, planta.precio)
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
                 pstmt.executeUpdate()
-                println("Planta '${planta.nombreComun}' insertada con éxito.")
+                println("Coche '${coche.modelo}' insertada con éxito.")
             }
         } ?: println("No se pudo establecer la conexión.")
     }
 
-    fun actualizarPlanta(planta: Coche) {
-        if (planta.id_planta == null) {
+    fun actualizarCoche(coche: Coche) {
+        if (coche.id_coche == null) {
             println("No se puede actualizar una planta sin id.")
             return
         }
         getConnection()?.use { conn ->
             conn.prepareStatement(
-                "UPDATE plantas SET nombre_comun = ?, nombre_cientifico = ?, stock = ?, precio = ? WHERE id_planta = ?"
+                "UPDATE coches SET modelo = ?, marca = ?, consumo = ?, hp = ? WHERE id_coche = ?"
             ).use { pstmt ->
-                pstmt.setString(1, planta.nombreComun)
-                pstmt.setString(2, planta.nombreCientifico)
-                pstmt.setInt(3, planta.stock)
-                pstmt.setDouble(4, planta.precio)
-                pstmt.setInt(5, planta.id_planta)
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
+                pstmt.setInt(5, coche.id_coche)
                 val filas = pstmt.executeUpdate()
                 if (filas > 0) {
-                    println("Planta con id=${planta.id_planta} actualizada con éxito.")
+                    println("Coche con id=${coche.id_coche} actualizado con éxito.")
                 } else {
-                    println("No se encontró ninguna planta con id=${planta.id_planta}.")
+                    println("No se encontró ningun coche con id=${coche.id_coche}.")
                 }
             }
         } ?: println("No se pudo establecer la conexión.")
     }
 
-    fun eliminarPlanta(id: Int) {
+    fun eliminarCoche(id: Int) {
         getConnection()?.use { conn ->
-            conn.prepareStatement("DELETE FROM plantas WHERE id_planta = ?").use { pstmt ->
+            conn.prepareStatement("DELETE FROM coches WHERE id_coche = ?").use { pstmt ->
                 pstmt.setInt(1, id)
                 val filas = pstmt.executeUpdate()
                 if (filas > 0) {
-                    println("Planta con id=$id eliminada correctamente.")
+                    println("Coche con id=$id eliminada correctamente.")
                 } else {
-                    println("No se encontró ninguna planta con id=$id.")
+                    println("No se encontró ningun coche con id=$id.")
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+}
+
+object RuedasDAO {
+
+    fun listarCoches(): List<Coche> {
+        val lista = mutableListOf<Coche>()
+        getConnection()?.use { conn ->
+            conn.createStatement().use { stmt ->
+                val rs = stmt.executeQuery("SELECT * FROM coches")
+                while (rs.next()) {
+                    lista.add(
+                        Coche(
+                            id_coche = rs.getInt("id_coche"),
+                            modelo = rs.getString("modelo"),
+                            marca = rs.getString("marca"),
+                            consumo  = rs.getDouble("consumo"),
+                            hp = rs.getInt("hp")
+                        )
+                    )
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+        return lista
+    }
+
+    fun consultarCochePorId(id: Int): Coche? {
+        var coche: Coche? = null
+        getConnection()?.use { conn ->
+            conn.prepareStatement("SELECT * FROM coches WHERE id_coche = ?").use { pstmt ->
+                pstmt.setInt(1, id)
+                val rs = pstmt.executeQuery()
+                if (rs.next()) {
+                    coche = Coche(
+                        id_coche = rs.getInt("id_coche"),
+                        modelo = rs.getString("modelo"),
+                        marca = rs.getString("marca"),
+                        consumo = rs.getDouble("consumo"),
+                        hp = rs.getInt("hp")
+                    )
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+        return coche
+    }
+
+    fun insertarCoche(coche: Coche) {
+        getConnection()?.use { conn ->
+            conn.prepareStatement(
+                "INSERT INTO coches(modelo, marca, consumo, hp) VALUES (?, ?, ?, ?)"
+            ).use { pstmt ->
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
+                pstmt.executeUpdate()
+                println("Coche '${coche.modelo}' insertada con éxito.")
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+
+    fun actualizarCoche(coche: Coche) {
+        if (coche.id_coche == null) {
+            println("No se puede actualizar una planta sin id.")
+            return
+        }
+        getConnection()?.use { conn ->
+            conn.prepareStatement(
+                "UPDATE coches SET modelo = ?, marca = ?, consumo = ?, hp = ? WHERE id_coche = ?"
+            ).use { pstmt ->
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
+                pstmt.setInt(5, coche.id_coche)
+                val filas = pstmt.executeUpdate()
+                if (filas > 0) {
+                    println("Coche con id=${coche.id_coche} actualizado con éxito.")
+                } else {
+                    println("No se encontró ningun coche con id=${coche.id_coche}.")
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+
+    fun eliminarCoche(id: Int) {
+        getConnection()?.use { conn ->
+            conn.prepareStatement("DELETE FROM coches WHERE id_coche = ?").use { pstmt ->
+                pstmt.setInt(1, id)
+                val filas = pstmt.executeUpdate()
+                if (filas > 0) {
+                    println("Coche con id=$id eliminada correctamente.")
+                } else {
+                    println("No se encontró ningun coche con id=$id.")
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+}
+object RepuestoDAO {
+
+    fun listarCoches(): List<Coche> {
+        val lista = mutableListOf<Coche>()
+        getConnection()?.use { conn ->
+            conn.createStatement().use { stmt ->
+                val rs = stmt.executeQuery("SELECT * FROM coches")
+                while (rs.next()) {
+                    lista.add(
+                        Coche(
+                            id_coche = rs.getInt("id_coche"),
+                            modelo = rs.getString("modelo"),
+                            marca = rs.getString("marca"),
+                            consumo  = rs.getDouble("consumo"),
+                            hp = rs.getInt("hp")
+                        )
+                    )
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+        return lista
+    }
+
+    fun consultarCochePorId(id: Int): Coche? {
+        var coche: Coche? = null
+        getConnection()?.use { conn ->
+            conn.prepareStatement("SELECT * FROM coches WHERE id_coche = ?").use { pstmt ->
+                pstmt.setInt(1, id)
+                val rs = pstmt.executeQuery()
+                if (rs.next()) {
+                    coche = Coche(
+                        id_coche = rs.getInt("id_coche"),
+                        modelo = rs.getString("modelo"),
+                        marca = rs.getString("marca"),
+                        consumo = rs.getDouble("consumo"),
+                        hp = rs.getInt("hp")
+                    )
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+        return coche
+    }
+
+    fun insertarCoche(coche: Coche) {
+        getConnection()?.use { conn ->
+            conn.prepareStatement(
+                "INSERT INTO coches(modelo, marca, consumo, hp) VALUES (?, ?, ?, ?)"
+            ).use { pstmt ->
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
+                pstmt.executeUpdate()
+                println("Coche '${coche.modelo}' insertada con éxito.")
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+
+    fun actualizarCoche(coche: Coche) {
+        if (coche.id_coche == null) {
+            println("No se puede actualizar una planta sin id.")
+            return
+        }
+        getConnection()?.use { conn ->
+            conn.prepareStatement(
+                "UPDATE coches SET modelo = ?, marca = ?, consumo = ?, hp = ? WHERE id_coche = ?"
+            ).use { pstmt ->
+                pstmt.setString(1, coche.modelo)
+                pstmt.setString(2, coche.marca)
+                pstmt.setDouble(3, coche.consumo)
+                pstmt.setInt(4, coche.hp)
+                pstmt.setInt(5, coche.id_coche)
+                val filas = pstmt.executeUpdate()
+                if (filas > 0) {
+                    println("Coche con id=${coche.id_coche} actualizado con éxito.")
+                } else {
+                    println("No se encontró ningun coche con id=${coche.id_coche}.")
+                }
+            }
+        } ?: println("No se pudo establecer la conexión.")
+    }
+
+    fun eliminarCoche(id: Int) {
+        getConnection()?.use { conn ->
+            conn.prepareStatement("DELETE FROM coches WHERE id_coche = ?").use { pstmt ->
+                pstmt.setInt(1, id)
+                val filas = pstmt.executeUpdate()
+                if (filas > 0) {
+                    println("Coche con id=$id eliminada correctamente.")
+                } else {
+                    println("No se encontró ningun coche con id=$id.")
                 }
             }
         } ?: println("No se pudo establecer la conexión.")
